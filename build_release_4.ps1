67865b4a9fb1c94a62ae03bb2838197c9a3dc518
Sky Morey <moreys2@digitalev.com>
2012-07-09 16:45:25 -0500

    updated psake build to support x87, x64 and UAC releases

--- build_release_4.ps1
+++ build_release_4.ps1
@@ -1,3 +1,6 @@
 import-module .\psake.psm1
 invoke-psake -framework '4.0' .\default.ps1 -properties @{config='Release';target_framework_version='4.0'}
+$base_dir = resolve-path .
+invoke-psake -framework '4.0' .\default.ps1 -properties @{config='Release';platform='x86';target_framework_version='4.0';build_dir="$base_dir\build\x86";buildartifacts_dir="$base_dir\build\x86\";release_dir="$base_dir\Release\x86\"}
+invoke-psake -framework '4.0' .\default.ps1 -properties @{config='Release';platform='x64';target_framework_version='4.0';build_dir="$base_dir\build\x64";buildartifacts_dir="$base_dir\build\x64\";release_dir="$base_dir\Release\x64\"}
 remove-module psake
